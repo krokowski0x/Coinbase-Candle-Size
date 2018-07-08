60615f0d76051923aca20b48c90ad7eb62f2a624
krokowski0x <krokowski.dx@gmail.com>
2018-07-08 14:40:17 +0200

    Refactored CandleSize component to use Candle instead of JSX

--- src/CandleSize.js
+++ src/CandleSize.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-const APIURL = 'https://api-public.sandbox.pro.coinbase.com/products/BTC-USD/candles';
+import Candle from './Candle.js'
+import './CandleSize.css';
+
+const APIURL = 'https://api-public.sandbox.pro.coinbase.com/products/';
 
 export default class CandleSize extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      candles: {},
+      candles: [],
+      currency: 'BTC',
+      baseCurrency: 'USD',
     };
   }
 
   componentDidMount() {
-    fetch(APIURL, {
-      granularity: 3600
-    })
+    const { currency, baseCurrency } = this.state;
+
+    fetch(`${APIURL}${currency}-${baseCurrency}/candles?granularity=3600`)
     .then(res => res.json())
     .then(candles => this.setState({ candles }));
   }
 
   render() {
+    const { candles, currency, baseCurrency } = this.state;
+
     return (
-      <div>
-        <h2>Candles</h2>
+      <div className="CandleSize">
+        {candles.map(candle =>
+          <Candle
+            time={candle[0]}
+            low={candle[1]}
+            high={candle[2]}
+            open={candle[3]}
+            close={candle[4]}
+          />
+        )}
       </div>
     );
   }
