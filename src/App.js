998070985d645eb501277cd1b6de9b79ce0bcd6b
krokowski0x <krokowski.dx@gmail.com>
2018-07-13 14:58:55 +0200

    Changed extension folder structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
 import React, { Component } from 'react';
-import CandleChart from './CandleChart.js';
+import Candle from './Candle.js'
 
-class App extends Component {
+const APIURL = 'https://api-public.sandbox.pro.coinbase.com/products/';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      candles: [],
+      currency: 'BTC',
+      baseCurrency: 'USD',
+    };
+  };
+
+  filterOutWeirdCandles(candles) {
+    return candles.filter(candle => (candle[1] > 1 && candle[2] < 50000));
+  };
+
+  getPriceBoundries(candles) {
+    let min = Infinity;
+    let max = -Infinity;
+    candles.forEach(candle => {
+      if (candle[1] < min) min = candle[1];
+      if (candle[2] > max) max = candle[2];
+    })
+    this.setState({ candles, max, min })
+  };
+
+  componentDidMount() {
+    const { currency, baseCurrency } = this.state;
+
+    fetch(`${APIURL}${currency}-${baseCurrency}/candles?granularity=3600`)
+    .then(res => res.json())
+    .then(candles => this.filterOutWeirdCandles(candles))
+    .then(candles => this.getPriceBoundries(candles));
+  };
+
   render() {
+    const { candles, currency, baseCurrency, max, min } = this.state;
+
     return (
       <div className="App">
-        <h1 className="App-title">Coinbase Candle Size Indicator</h1>
-        <CandleChart />
+        <header></header>
+        <main>
+          <button>Click me!</button>
+          <button>Click me!</button>
+          <button>Click me!</button>
+        </main>
+        <footer></footer>
       </div>
     );
-  }
-}
-
-export default App;
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles.css';
+import './public/styles.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
